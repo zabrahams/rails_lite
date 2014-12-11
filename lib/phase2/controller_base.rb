477c2d6c53658afceeec848b44627a3f6ca6fc55
Zach Abrahams <zachabrahams@gmail.com>
2014-12-11 11:45:50 -0500

    Completes Phase 2 - simple ControllerBase

--- lib/phase2/controller_base.rb
+++ lib/phase2/controller_base.rb
@@ -4,20 +4,30 @@ module Phase2
 
     # Setup the controller
     def initialize(req, res)
+      @req, @res = req, res
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      @already_built_response || false
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      raise "Already Rendered" if already_built_response?
+      @already_built_response = true
+      @res.status = 302
+      @res['Location'] = url
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, type)
+      raise "Already Rendered" if already_built_response?
+      @already_built_response = true
+      @res['Content-Type'] = type
+      @res.body = content
     end
   end
 end
