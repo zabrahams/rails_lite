0abb6eeb45d1590aca6695eccab329120aa22847
Zach Abrahams <zachabrahams@gmail.com>
2014-12-11 18:35:06 -0500

    Adds link_to and button_to.

--- lib/bonus/url_helper.rb
+++ lib/bonus/url_helper.rb
@@ -1,12 +1,33 @@
-module UrlHelper
+require 'htmlentities'
+
+module ApplicationHelper
 
   def link_to(text, url)
     "<a href='#{html_escape(url)}'>#{html_escape(text)}</a>"
   end
 
-  def button_to(text, url, method)
-
+  def button_to(text, url, method="POST")
+    <<-HTML
+    <form action="#{html_escape(url)}" method="POST">
+      <input type="hidden" name="_method" value="#{html_escape(method)}">
+      <input type="submit" value"#{html_escape(text)}">
+    </form>
+    HTML
   end
+
+  def html_escape(text)
+    coder = HTMLEntities.new
+    coder.encode(text)
+  end
+
+
+end
+
+module UrlHelper
+
+
+
+
 
 
 end
